1a1ec78a786b9321085ca53777496b7dca62090c
Miguel da Silva Pinto <miguel.silva48@ua.pt>
2024-05-12 21:59:07 +0100

    Chore(TP-14-Create-Connection): rearranged service test directory

--- backend/chuchu/src/test/java/tqsgroup/chuchu/service/CarriageServiceTest.java
+++ backend/chuchu/src/test/java/tqsgroup/chuchu/service/CarriageServiceTest.java
@@ -1,4 +1,4 @@
-package tqsgroup.chuchu.unit.service;
+package tqsgroup.chuchu.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- backend/chuchu/src/test/java/tqsgroup/chuchu/service/ConnectionServiceTest.java
+++ backend/chuchu/src/test/java/tqsgroup/chuchu/service/ConnectionServiceTest.java
@@ -1,4 +1,4 @@
-package tqsgroup.chuchu.unit.service;
+package tqsgroup.chuchu.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
