89f07dcf0ae005d18b5b4da676422daccee5b37c
DiogoMMarto <diogo.marto@ua.pt>
2024-05-30 17:55:57 +0100

    Chore: Start

---
+++ backend/chuchu/src/main/java/tqsgroup/chuchu/publicApi/PublicRestApi.java
@@ -0,0 +1,10 @@
+package tqsgroup.chuchu.publicAPi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/api/v1")
+public class PublicRestApi {
+    
+}
