26549461a7a5afc447ec3fa4b344a3df1c9e3439
uTigas <tfgp@ua.pt>
2024-05-30 18:23:54 +0100

    Hotfix: Fixed bug where One Test Station was not deleted

--- backend/chuchu/src/test/java/tqsgroup/chuchu/databases/Neo4jDatabaseTest.java
+++ backend/chuchu/src/test/java/tqsgroup/chuchu/databases/Neo4jDatabaseTest.java
@@ -26,6 +26,6 @@ public class Neo4jDatabaseTest {
         // Perform a simple query to verify the connection
         assertEquals(stationRepository.findAll().size(), prevSize + 1);
         
-        stationRepository.deleteById(stationRepository.findByName("Test Station").getId());
+        stationRepository.deleteAll();
     }
 }
